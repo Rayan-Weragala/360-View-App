9f342939fbcbd562b1ed1226f8f112d0eef34c9e
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2024-03-19 22:37:23 +0530

    created server side and connected to the database

---
+++ Server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connected successfully!");
+});
+
+app.listen(PORT, () => {
+  console.log(`\nServer is running on port ${PORT}`);
+});
