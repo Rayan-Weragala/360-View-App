2cc02b99a76c1a0f1112dba36926fdd46da7e112
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2024-03-19 21:40:24 +0530

    Made sound effects for the 360 view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import SphereViewer from "./SphereViewer";
 import imageUrl1 from "./images/forest.jpg"; // Assuming imageUrl1 and imageUrl2 are imported from different files
-import imageUrl2 from "./images/photo2.jpg";
+import image from "./images/photo2.jpg";
 
 function App() {
   return (
     <div className="App">
       
       <div className="container">
-        <SphereViewer imageUrl={imageUrl2} />
+        <SphereViewer imageUrl={image} />
       </div>
     </div>
   );
--- src/SphereViewer.js
+++ src/SphereViewer.js
@@ -1,19 +1,47 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef,useState } from "react";
 import { Viewer } from "@photo-sphere-viewer/core";
 import "@photo-sphere-viewer/core/index.css";
 import { AutorotatePlugin } from "@photo-sphere-viewer/autorotate-plugin";
-import { CompassPlugin } from '@photo-sphere-viewer/compass-plugin';
+import { CompassPlugin } from "@photo-sphere-viewer/compass-plugin";
 import "@photo-sphere-viewer/compass-plugin/index.css";
+import { MapPlugin } from "@photo-sphere-viewer/map-plugin";
+import "@photo-sphere-viewer/map-plugin/index.css";
+import { GalleryPlugin } from "@photo-sphere-viewer/gallery-plugin";
+import "@photo-sphere-viewer/gallery-plugin/index.css";
+import image3 from "./images/forest.jpg";
+import image4 from "./images/photo2.jpg";
+import image from "./images/photo2.jpg";
+import audio from "./music/hevisi.mp3";
+import "./CSS/style.css";
+
 
 const SphereViewer = ({ imageUrl }) => {
   const viewerRef = useRef(null);
+  const [muted, setMuted] = useState(false);
 
   useEffect(() => {
     if (viewerRef.current) {
       const viewer = new Viewer({
         container: viewerRef.current,
         panorama: imageUrl,
-        navbar: true,
+        navbar: [
+          "zoom",
+          {
+            id: "custom-button",
+            content:
+              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-speaker"><rect x="4" y="2" width="16" height="20" rx="2" ry="2"></rect><circle cx="12" cy="14" r="4"></circle><line x1="12" y1="6" x2="12" y2="6"></line></svg>',
+            title: "mute",
+            className: "custom-icon-button",
+            onClick: () => {
+              setMuted(!muted);
+            },
+          },
+          "autorotate",
+          "gallery",
+          "download",
+          "caption",
+          "fullscreen",
+        ],
         plugins: [
           [
             AutorotatePlugin,
@@ -28,17 +56,59 @@ const SphereViewer = ({ imageUrl }) => {
               position: "top left",
               navigation: true,
             },
+          ],
+          [
+            MapPlugin,
+            {
+              imageUrl: image,
+              center: { x: 785, y: 421 },
+              rotation: "125deg",
+              defaultZoom: 40,
+            },
+          ],
+          [
+            GalleryPlugin,
+            {
+              items: [
+                {
+                  id: "pano-1",
+                  name: "Panorama 1",
+                  panorama: image3,
+                  thumbnail: image3,
+                },
+                {
+                  id: "pano-2",
+                  name: "Panorama 2",
+                  panorama: image4,
+                  thumbnail: image4,
+                },
               ],
-            ]
-            
-    
+            },
+          ],
+        ],
       });
 
       return () => viewer.destroy();
     }
-  }, [imageUrl]);
+  }, [imageUrl, muted]);
 
-  return <div ref={viewerRef} style={{ width: "50vw", height: "50vh",position:"absolute" }} />;
+  return (
+    <div
+      ref={viewerRef}
+      style={{
+        width: "100%",
+        height: "80vh",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      {muted ? null : (
+        <audio src={audio} autoPlay loop controls className="audio-player" />
+      )}
+    </div>
+  );
 };
 
 export default SphereViewer;
